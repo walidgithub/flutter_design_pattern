0e071ec9c3e3055a774d0375abba84e0d835e36f
walid barakat <walidmohamedbarakat@gmail.com>
2023-10-23 18:10:09 +0300

    prototype pattern

--- lib/design_patterns/prototype/shape.dart
+++ lib/design_patterns/prototype/shape.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
+// this is the prototype class
 abstract class Shape {
   Shape(this.color);
 
+  // here it is a named constructor with default value for variable color from source
   Shape.clone(Shape source) : color = source.color;
 
   Color color;
--- lib/design_patterns/prototype/shapes/circle.dart
+++ lib/design_patterns/prototype/shapes/circle.dart
@@ -3,17 +3,26 @@ import 'package:flutter/material.dart';
 
 import '../shape.dart';
 
+// this is a concrete prototype class
 class Circle extends Shape {
+  double radius;
+
+  // here it is a constructor with variable radius and inherited variable color
   Circle(super.color, this.radius);
 
+  // here it is a named constructor with default value for variable radius
+  // and with default value for inherited variable color
   Circle.initial([super.color = Colors.black]) : radius = 50.0;
 
+  // this named constructor inherit superclass named constructor by using super.clone
+  // here it is a named constructor with default value for variable radius from source
+  // so it will have color from superclass (it was a default value too) and radius from as default value
+  // you will send original class Circle with its variables color and radius as a parameter
   Circle.clone(Circle super.source)
       : radius = source.radius,
         super.clone();
 
-  double radius;
-
+  // this function will run the named constructor (clone) and return a clone from Circle class with its current variables color and radius values
   @override
   Shape clone() => Circle.clone(this);
 
